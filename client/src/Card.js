1a2aefe19753fa44d03ef810735501a0105d5c60
Andrew Keown <andrew.keown@enable.com>
2023-07-08 19:54:41 +0100

    ui

--- client/src/Card.js
+++ client/src/Card.js
@@ -9,7 +9,7 @@ const Card = ({type, width}) => {
         'Spider': "./cards/card_spider.png",
         'Stink Bug': "./cards/card_stinkbug.png"
     }
-    return <img style={{width: width}} src={map[type]} alt={type}></img>
+    return <img className='face-card' style={{width: width}} src={map[type]} alt={type}></img>
 } 
 
 export default Card;
