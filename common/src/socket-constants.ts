22be0fc89ad59cb16fe0c8754abd419cc528c817
Oliver Moran <oliver.moran@enable.com>
2023-07-08 14:33:33 +0100

    f

--- common/src/socket-constants.ts
+++ common/src/socket-constants.ts
@@ -1,2 +1,5 @@
 export const GAME_STATE = 'game state';
 export const SET_NAME = 'set name';
+export const CHAT_MESSAGE = 'chat message';
+export const PLAYER_DISCONNECT = 'disconnect';
+export const SPECTATOR_DISCONNECT = 'disconnect';
