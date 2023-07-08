0102cd9ee6a70057f6a6f4fc0914e7960d289d49
Oliver Moran <oliver.moran@enable.com>
2023-07-08 09:58:11 +0100

    Fix stuff

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+// Create Express app
+const app = (0, express_1.default)();
+const port = 6969;
+// Define routes
 app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+    res.send('Hello, TypeScript with Express!!!');
 });
-
+// Start the server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
