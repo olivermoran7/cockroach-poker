05bd41fb79a9899df68f5f16e8f2b488cd79e3e1
Charlie Merrell <charlie.merrell@enable.com>
2023-07-08 15:04:03 +0100

    More edits

--- client/src/Card.js
+++ client/src/Card.js
@@ -1,3 +1,16 @@
-const Card = (card) => {
-    return 
+const Card = ({type}) => {
+    console.log(type)
+    const map = {
+        'Cockroach': "./cards/card_cockroach.png",
+        'Bat': "./cards/card_bat.png",
+        'Fly': "./cards/card_fly.png",
+        'Toad': "./cards/card_toad.png",
+        'Rat': "./cards/card_rat.png",
+        'Scorpion': "./cards/card_scorpion.png",
+        'Spider': "./cards/card_spider.png",
+        'Stink Bug': "./cards/card_stinkbug.png"
+    }
+    return <img style={{width: "32px"}} src={map[type]} alt={type}></img>
 } 
+
+export default Card;
