df591a85c3f2d04241c554263c90bea79665c48b
Andrew Keown <andrew.keown@enable.com>
2023-07-08 19:19:40 +0100

    fix lobby

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,8 @@ import io from 'socket.io-client';
 import Player from './Player';
 import CardTargeter from './CardTargeter';
 import Card from './Card';
+import ReactPlayer from 'react-player';
 import ChatBox from './ChatBox';
-import Lobby from './Lobby';
 
 const startSate = {
   players: [
@@ -126,69 +126,89 @@ function App() {
   const activePlayer = () => gameState.players.find(player => player.connection === gameState.playerTurn[gameState.playerTurn.length - 1]);
   const targetPlayer = () => gameState.players.find(player => player.connection === gameState.play.targetPlayer);
   const offeredCard = () => gameState.play.purportedCard;
-
-  return <Lobby />
-
+  
+  var [sniffin, setSniffin] = useState(true);
   if (gameState === undefined) {
     return <p>loading...</p>
   }
   else {
-    return ( 
-      <>
-        {/* Start game */}
-        {
-          gameState.inLobby && gameState.players.length > 1 &&
-          <button onClick={onClickStartGame}>Start game</button>
-        }
-
-
-        {/* Opponents */}
-        <h1>{gameState.inLobby ? "Lobby" : "In game"}</h1>
-        <div style={{"display": "flex", "justifyContent": "space-between" }}>
-          {opponents().map(player => {
-            return <Player name = {player.name} typeCount={countCardTypes(player.cardsFaceUp)} />
-          })
-        }
-        </div>
-
-        {/* Play */}
-        {
-        gameState.play && 
-        <div>
-          <p>{activePlayer().name} offers {targetPlayer().name} a {offeredCard().type}</p>
-          
-        </div>
-
-
-        }
-
-        {/* <ChatBox></ChatBox> */}
-
-        {/* Selector */}
-        {
-          selectedCard && <CardTargeter players={opponents()} onConfirm={onSendCard} />
-        }
-
-        {/* Responder */}
-        {meTargeted() && 
-        <div>
-          <button onClick={onClickTrust}>Trust</button>
-          <button onClick={onClickCallBluff}>Call bluff</button>
-        </div>
-       }
-
-        {/* My state */}
-        {me() &&
+    if (sniffin) {
+      return (
+      <div className="lobby">
+        <img
+          className="background-image"
+          src="./portsniffer.png" 
+          alt="Fullscreen Image"
+        />
+        <ReactPlayer
+          url="./Project 51.mp3"
+          playing
+          loop
+          volume={0.8}
+          width="0"
+          height="0"
+        />
+      <button onClick={() => setSniffin(false)}>Start sniffin'</button>
+      </div>
+      )
+    } else {
+      return ( 
         <>
-        <Player name = {me().name} typeCount={countCardTypes(me().cardsFaceUp)} />
-        <div style={{display: "flex"}}>
-          {me().cardsInHand.map(card => <div style={{cursor: "pointer"}} onClick={() => onClickSelectCard(card)}><Card type={card.type} /></div>)}
-        </div>
-        </>
+          {/* Start game */}
+          {
+            gameState.inLobby && gameState.players.length > 1 &&
+            <button onClick={onClickStartGame}>Start game</button>
+          }
+
+
+          {/* Opponents */}
+          <h1>{gameState.inLobby ? "Lobby" : "In game"}</h1>
+          <div style={{"display": "flex", "justifyContent": "space-between" }}>
+            {opponents().map(player => {
+              return <Player name = {player.name} typeCount={countCardTypes(player.cardsFaceUp)} />
+            })
+          }
+          </div>
+
+          {/* Play */}
+          {
+          gameState.play && 
+          <div>
+            <p>{activePlayer().name} offers {targetPlayer().name} a {offeredCard().type}</p>
+            
+          </div>
+
+
+          }
+
+          {/* <ChatBox></ChatBox> */}
+
+          {/* Selector */}
+          {
+            selectedCard && <CardTargeter players={opponents()} onConfirm={onSendCard} />
+          }
+
+          {/* Responder */}
+          {meTargeted() && 
+          <div>
+            <button onClick={onClickTrust}>Trust</button>
+            <button onClick={onClickCallBluff}>Call bluff</button>
+          </div>
         }
 
-      </>
-    );
+          {/* My state */}
+          {me() &&
+          <>
+          <Player name = {me().name} typeCount={countCardTypes(me().cardsFaceUp)} />
+          <div style={{display: "flex"}}>
+            {me().cardsInHand.map(card => <div style={{cursor: "pointer"}} onClick={() => onClickSelectCard(card)}><Card type={card.type} /></div>)}
+          </div>
+          </>
+          }
+
+        </>
+      );
+    }
   }
 }
 
