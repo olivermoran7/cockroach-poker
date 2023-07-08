38401ad26ad05d8b271c2e21f218719ee485e1a8
Charlie Merrell <charlie.merrell@enable.com>
2023-07-08 11:42:37 +0100

    Update client

--- cockroach-client/src/App.tsx
+++ cockroach-client/src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import io from 'socket.io-client';
+import { GAME_STATE } from 'common/src/socket-constants'
+
+import gameState from 'common/src/gameState';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [gameState, setGameState] = useState<gameState | undefined>(undefined);
+
+  useEffect(() => {
+    // Create a Socket.IO client instance
+    const socket = io('http://localhost:6969'); 
+
+    // Event handler for receiving messages
+    socket.on("GAME_STATE", (message: string) => {
+      var newGameState: gameState = JSON.parse(message);
+      setGameState(newGameState);
+    });
+
+    // Clean up the socket connection on component unmount
+    return () => {
+
+      socket.disconnect();
+    };
+  }, []);
+
+  if (gameState === undefined) {
+    return <p>loading...</p>
+  }
+  else {
+    return (
+      <>
+        <h1>{gameState.inLobby ? "Lobby" : "In game"}</h1>
+      </>
+    );
+  }
 }
 
 export default App;
