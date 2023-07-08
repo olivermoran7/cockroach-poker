1343a89285d7fdbbc287d88791451df1725b98f9
Regan Hughes <Regan Hughes>
2023-07-08 12:00:23 +0100

    Add makeId

--- api/game-service/game-service.ts
+++ api/game-service/game-service.ts
@@ -29,4 +29,14 @@ export class GameService {
 
 		player.name = name;
 	}
+
+	private makeId(length: number) {
+		var result = '';
+		var characters = '0123456789';
+		var charactersLength = characters.length;
+		for ( var i = 0; i < length; i++ ) {
+			result += characters.charAt(Math.floor(Math.random() * charactersLength));
+		}
+	   return result;
+	}
 }
