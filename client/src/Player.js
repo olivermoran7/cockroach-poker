4f0998fbb005f119b729583e6df5efeda53fd42e
Andrew Keown <andrew.keown@enable.com>
2023-07-10 12:24:10 +0100

    yes

--- client/src/Player.js
+++ client/src/Player.js
@@ -5,7 +5,7 @@ var Player = ({ name, typeCount, opponent, activePlayer }) => {
         <p className={activePlayer === name ? "player-name active" : "player-name"}>{name}</p>
         <img className="card-image" src="./cardback.png" alt="Card Back" />
         <img className="card-image" style={{"marginTop": "calc(var(--_width) * -1.72)"}} src="./cardback.png" />
-        <img className="card-image" style={{"marginTop": "calc(var(--_width) * -1.72)"}} src="./cardback.png" />
+        <img className="card-image" style={{"marginTop": "calc(var(--_width) * -1.72)", "box-shadow": "10px 11px 5px #0f0f0f85"}} src="./cardback.png" />
         {typeCount.map((typeCount) => {
           return (
             <div key={typeCount.type}>
@@ -20,7 +20,7 @@ var Player = ({ name, typeCount, opponent, activePlayer }) => {
       <div className="player-container">
         <img className="card-image" src="./cardback.png" alt="Card Back" />
         <img className="card-image" style={{"marginTop": "calc(var(--_width) * -1.59)"}} src="./cardback.png" />
-        <img className="card-image" style={{"marginTop": "calc(var(--_width) * -1.59)"}} src="./cardback.png" />
+        <img className="card-image" style={{"marginTop": "calc(var(--_width) * -1.59)", "box-shadow": "16px 14px 5px #0f0f0f85"}} src="./cardback.png" />
         {typeCount.map((typeCount) => {
           return (
             <div key={typeCount.type}>
